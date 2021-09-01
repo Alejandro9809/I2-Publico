b108cac8cd6cf1832bffebc6a2bf018ed9e03d0d
Alejandro9809 <alejandro.980904@gmail.com>
2021-09-01 11:20:18 -0500

    Cambios en un codigo

--- lib/src/primo.c
+++ lib/src/primo.c
@@ -1,7 +1,7 @@
 #include <stdbool.h>
-/** @brief A Function
-* @param firs number
-* @returns   flag_return 
+/** @brief  Recibe un numero y devuelve si es primo o no 
+* @param    number primer numero
+* @returns   bool flag return si es primo o no  
 */
 
 
