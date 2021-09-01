e19bcf512bf8df19383f8f31a61c1ec5a0360c6e
RodrigoGarciaNu <rodrigognunez@outlook.com>
2021-08-31 21:59:33 -0500

    La funcion que eleva un numero dado a un exponente dado funciona correctamente

---
+++ lib/src/Exponente.c
@@ -0,0 +1,13 @@
+/** @brief Funcion que eleva un numero elevado a un exponente
+ * @param x  Primer parametro
+ * @param y Segundo parametro
+ * @returns x
+*/
+
+double Exponencial (double x, double y){
+    int z = x;
+    for (int cont = 1; cont < y; cont++){
+        x = x * z;
+    }
+    return x;
+}
