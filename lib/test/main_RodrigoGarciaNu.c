c56674877058693911c6f270afd7d762ee400e04
RodrigoGarciaNu <rodrigognunez@outlook.com>
2021-09-01 17:27:26 -0500

    Prueba de biblioteca

--- lib/test/main_RodrigoGarciaNu.c
+++ lib/test/main_RodrigoGarciaNu.c
@@ -6,9 +6,22 @@ int main(int argc, char const *argv[]){
 
     double op1 = atoi(argv[1]);   //pedimos memoria dinámica para los parametros que se utilzarán posteriormenete
     double op2 = atoi(argv[2]);   //en las funciones
-    double z = 5;
+    double z;
     z = Exponencial(op1, op2);
-    printf("holaaa %.f", z);
+    printf("%.f \n", z);
+
+    z = factorial(op1);
+    printf("%.f \n", z);
+
+    z = porcentaje(op1, op2);
+    printf("%.f \n", z);
+
+    if(es_primo(op1)){
+        printf("%.f es primo\n", op1);
+    }
+    else{
+        printf("%.f no es primo\n", op1);
+    }
 
     return 0;
 }
