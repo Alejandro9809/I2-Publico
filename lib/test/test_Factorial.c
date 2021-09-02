842f1747027ea487a655ff0b67d307cc4f4de3b6
Alejandro9809 <alejandro.980904@gmail.com>
2021-09-02 01:05:50 -0500

    Borrado

--- lib/test/test_Factorial.c
+++
@@ -1,9 +0,0 @@
-#include "../src/Factorial.c"
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char const *argv[]){
-    int x= atoi(argv[1]);
-    printf("%.f\n",factorial(x));
-    return 0;
-}
